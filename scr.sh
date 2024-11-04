6782af0c8514ad084601438cebd4db04700fb9f1
edtell <yournewemail@example.com>
2024-11-04 18:46:45 +0200

    sdaf

--- scr.sh
+++ scr.sh
@@ -6,6 +6,18 @@ prev_value=""
 out=$(mmcli -L)
 m_id=$(echo "$out" | grep -oP '/org/freedesktop/ModemManager1/Modem/\K\d+')
 PID=null
+
+# Function to handle cleanup on exit
+cleanup() {
+	if [[ -n "$PID" && "$PID" != "null" ]]; then
+		kill $PID
+	fi
+	exit 0
+}
+
+# Trap SIGINT (Ctrl+C) and call the cleanup function
+trap cleanup SIGINT
+
 while true; do
 
 	current_value=$(mmcli -m "$m_id" --command='AT+QENG="servingcell"' | grep '+QENG: "LTE"' | awk -F, '{mcc=$3; mnc=$4; cellid=$5; print mcc, mnc, cellid}' | while read -r mcc mnc cellid; do printf "%s %s %d\n" "$mcc" "$mnc" "$((16#$cellid))"; done)
@@ -19,12 +31,12 @@ while true; do
 
 	if [[ "$current_value" != "$prev_value" ]]; then
 
-		/home/taltech/SUPL-3GPP-LPP-client/build/example-lpp osr -f rtcm -h 129.192.82.125 -p 5431 --imsi=248010203229380 -c "$mcc" -n "$mnc" -t 1 -i 2 --tcp=192.168.3.1 --tcp-port=3000 &
+		/home/taltech/SUPL-3GPP-LPP-client/build/example-lpp osr -f rtcm -h 129.192.82.125 -p 5431 --imsi=248010203229380 -c "$mcc" -n "$mnc" -t 1 -i 2 --tcp=192.168.3.1 --tcp-port=3000 | tee -a out.txt &
 		PID=$!
 
 	else
 
-		/home/taltech/SUPL-3GPP-LPP-client/build/example-lpp osr -f rtcm -h 129.192.82.125 -p 5431 --imsi=248010203229380 -c "$mcc" -n "$mnc" -t 1 -i 1 --tcp=192.168.3.1 --tcp-port=3000 &
+		/home/taltech/SUPL-3GPP-LPP-client/build/example-lpp osr -f rtcm -h 129.192.82.125 -p 5431 --imsi=248010203229380 -c "$mcc" -n "$mnc" -t 1 -i 1 --tcp=192.168.3.1 --tcp-port=3000 | tee -a out.txt &
 		PID=$!
 
 	fi
