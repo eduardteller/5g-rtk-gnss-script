c18d3662a7176d5370ea8c624271d25f37ebda4b
5G-Modem <5gmodem@example.com>
2024-11-04 14:40:52 +0200

    initial

---
+++ scr.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+source conf.cfg
+
+prev_value=""
+
+out=$(mmcli -L)
+m_id=$(echo "$out" | grep -oP '/org/freedesktop/ModemManager1/Modem/\K\d+')
+
+while true; do
+
+current_value=$(mmcli -m "$m_id" --command='AT+QENG="servingcell"' | grep '+QENG: "LTE"' | awk -F, '{mcc=$3; mnc=$4; cellid=$5; print mcc, mnc, cellid}' | while read -r mcc mnc cellid; do printf "%s %s %d\n" "$mcc" "$mnc" "$((16#$cellid))"; done)
+
+read -r mcc mnc cellid <<< "$current_value"
+echo MCC:"$mcc" MNC:"$mnc" CELL_ID:"$cellid" -  $(date +"%T")
+
+#/home/taltech/SUPL-3GPP-LPP-client/build/example-lpp osr -f rtcm -h 129.192.82.125 -p 5431 --imsi=248010203229380 -c 248 -n 1 -t 1 -i 1 --tcp=192.168.3.1 --tcp-port=3000
+PID=$!
+
+if [[ "$current_value" != "$prev_value" ]]; then
+
+	read -r mcc mnc cellid <<< "$current_value"
+        echo MCC:"$mcc" MNC:"$mnc" CELL_ID:"$cellid" -  $(date +"%T")
+
+	prev_value="$current_value" # Update prev_value to the current value
+
+	#date +"%T"
+        kill $PID
+
+	/home/taltech/SUPL-3GPP-LPP-client/build/example-lpp osr -f rtcm -h 129.192.82.125 -p 5431 --imsi=248010203229380 -c "$mcc" -n "$mnc" -t 1 -i 2 --tcp=192.168.3.1 --tcp-port=3000 &
+        PID=$!
+fi
+
+sleep $TIME
+done
+
