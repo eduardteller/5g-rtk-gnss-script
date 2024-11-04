c088e749ddaadee45ef8fda806f0761e995804bf
Eduard Teller <eduardteller1@gmail.com>
2024-11-04 15:00:43 +0200

    changes

--- scr.sh
+++ scr.sh
@@ -5,31 +5,40 @@ prev_value=""
 
 out=$(mmcli -L)
 m_id=$(echo "$out" | grep -oP '/org/freedesktop/ModemManager1/Modem/\K\d+')
-
+PID=null
 while true; do
 
-current_value=$(mmcli -m "$m_id" --command='AT+QENG="servingcell"' | grep '+QENG: "LTE"' | awk -F, '{mcc=$3; mnc=$4; cellid=$5; print mcc, mnc, cellid}' | while read -r mcc mnc cellid; do printf "%s %s %d\n" "$mcc" "$mnc" "$((16#$cellid))"; done)
+	current_value=$(mmcli -m "$m_id" --command='AT+QENG="servingcell"' | grep '+QENG: "LTE"' | awk -F, '{mcc=$3; mnc=$4; cellid=$5; print mcc, mnc, cellid}' | while read -r mcc mnc cellid; do printf "%s %s %d\n" "$mcc" "$mnc" "$((16#$cellid))"; done)
 
-read -r mcc mnc cellid <<< "$current_value"
-echo MCC:"$mcc" MNC:"$mnc" CELL_ID:"$cellid" -  $(date +"%T")
+	if [[ "$current_value" != "$prev_value" ]]; then
 
-#/home/taltech/SUPL-3GPP-LPP-client/build/example-lpp osr -f rtcm -h 129.192.82.125 -p 5431 --imsi=248010203229380 -c 248 -n 1 -t 1 -i 1 --tcp=192.168.3.1 --tcp-port=3000
-PID=$!
+		read -r mcc mnc cellid <<<"$current_value"
+		echo MCC:"$mcc" MNC:"$mnc" CELL_ID:"$cellid" - $(date +"%T")
 
-if [[ "$current_value" != "$prev_value" ]]; then
+		if [[ -n "$PID" && "$PID" != "null" ]]; then
+			kill $PID
+		fi
 
-	read -r mcc mnc cellid <<< "$current_value"
-        echo MCC:"$mcc" MNC:"$mnc" CELL_ID:"$cellid" -  $(date +"%T")
+		/home/taltech/SUPL-3GPP-LPP-client/build/example-lpp osr -f rtcm -h 129.192.82.125 -p 5431 --imsi=248010203229380 -c "$mcc" -n "$mnc" -t 1 -i 2 --tcp=192.168.3.1 --tcp-port=3000 &
+		PID=$!
 
-	prev_value="$current_value" # Update prev_value to the current value
+		prev_value="$current_value" # Update prev_value to the current value
 
-	#date +"%T"
-        kill $PID
+	else
+		read -r mcc mnc cellid <<<"$current_value"
+		echo MCC:"$mcc" MNC:"$mnc" CELL_ID:"$cellid" - $(date +"%T")
 
-	/home/taltech/SUPL-3GPP-LPP-client/build/example-lpp osr -f rtcm -h 129.192.82.125 -p 5431 --imsi=248010203229380 -c "$mcc" -n "$mnc" -t 1 -i 2 --tcp=192.168.3.1 --tcp-port=3000 &
-        PID=$!
-fi
+		if [[ -n "$PID" && "$PID" != "null" ]]; then
+			kill $PID
+		fi
 
-sleep $TIME
+		/home/taltech/SUPL-3GPP-LPP-client/build/example-lpp osr -f rtcm -h 129.192.82.125 -p 5431 --imsi=248010203229380 -c "$mcc" -n "$mnc" -t 1 -i 1 --tcp=192.168.3.1 --tcp-port=3000 &
+		PID=$!
+
+		prev_value="$current_value"
+
+	fi
+
+	sleep $TIME
+
 done
-
