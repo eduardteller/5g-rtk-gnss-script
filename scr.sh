b5384fbadcd1478f97e2f7729e7b4e4bd19827c4
Eduard Teller <eduardteller1@gmail.com>
2024-11-04 15:05:16 +0200

    changes

--- scr.sh
+++ scr.sh
@@ -10,34 +10,26 @@ while true; do
 
 	current_value=$(mmcli -m "$m_id" --command='AT+QENG="servingcell"' | grep '+QENG: "LTE"' | awk -F, '{mcc=$3; mnc=$4; cellid=$5; print mcc, mnc, cellid}' | while read -r mcc mnc cellid; do printf "%s %s %d\n" "$mcc" "$mnc" "$((16#$cellid))"; done)
 
+	read -r mcc mnc cellid <<<"$current_value"
+	echo MCC:"$mcc" MNC:"$mnc" CELL_ID:"$cellid" - $(date +"%T")
+
+	if [[ -n "$PID" && "$PID" != "null" ]]; then
+		kill $PID
+	fi
+
 	if [[ "$current_value" != "$prev_value" ]]; then
-
-		read -r mcc mnc cellid <<<"$current_value"
-		echo MCC:"$mcc" MNC:"$mnc" CELL_ID:"$cellid" - $(date +"%T")
-
-		if [[ -n "$PID" && "$PID" != "null" ]]; then
-			kill $PID
-		fi
 
 		/home/taltech/SUPL-3GPP-LPP-client/build/example-lpp osr -f rtcm -h 129.192.82.125 -p 5431 --imsi=248010203229380 -c "$mcc" -n "$mnc" -t 1 -i 2 --tcp=192.168.3.1 --tcp-port=3000 &
 		PID=$!
 
-		prev_value="$current_value" # Update prev_value to the current value
-
 	else
-		read -r mcc mnc cellid <<<"$current_value"
-		echo MCC:"$mcc" MNC:"$mnc" CELL_ID:"$cellid" - $(date +"%T")
-
-		if [[ -n "$PID" && "$PID" != "null" ]]; then
-			kill $PID
-		fi
 
 		/home/taltech/SUPL-3GPP-LPP-client/build/example-lpp osr -f rtcm -h 129.192.82.125 -p 5431 --imsi=248010203229380 -c "$mcc" -n "$mnc" -t 1 -i 1 --tcp=192.168.3.1 --tcp-port=3000 &
 		PID=$!
 
-		prev_value="$current_value"
-
 	fi
+
+	prev_value="$current_value"
 
 	sleep $TIME
 
