34b31a745993db47e2098c5512ade70396fb0189
Eduard Teller <eduardteller1@gmail.com>
2024-11-27 15:26:50 +0200

    as

--- scr.sh
+++ scr.sh
@@ -42,12 +42,12 @@ while true; do
 	if [[ "$current_value" != "$prev_value" ]]; then
 
 		echo -e "\e[31mCell change detected\e[0m"
-		# /home/taltech/SUPL-3GPP-LPP-client/build/example-lpp osr -f rtcm -h 129.192.82.125 -p 5431 --imsi=248010203229380 -c "$mcc" -n "$mnc" -t "$tac" -i 2 --tcp=192.168.3.1 --tcp-port=3000 & #>output.txt 2>&1 &
-		# PID=$!
-		# else
+		/home/taltech/SUPL-3GPP-LPP-client/build/example-lpp osr -f rtcm -h 129.192.82.125 -p 5431 --imsi=248010203229380 -c "$mcc" -n "$mnc" -t "$tac" -i 2 --tcp=192.168.3.1 --tcp-port=3000 & #>output.txt 2>&1 &
+		PID=$!
+	else
 
-		# /home/taltech/SUPL-3GPP-LPP-client/build/example-lpp osr -f rtcm -h 129.192.82.125 -p 5431 --imsi=248010203229380 -c "$mcc" -n "$mnc" -t "$tac" -i 1 --tcp=192.168.3.1 --tcp-port=3000 & #>output.txt 2>&1 &
-		# PID=$!
+		/home/taltech/SUPL-3GPP-LPP-client/build/example-lpp osr -f rtcm -h 129.192.82.125 -p 5431 --imsi=248010203229380 -c "$mcc" -n "$mnc" -t "$tac" -i 1 --tcp=192.168.3.1 --tcp-port=3000 & #>output.txt 2>&1 &
+		PID=$!
 	fi
 
 	prev_value="$cell_id_dec"
